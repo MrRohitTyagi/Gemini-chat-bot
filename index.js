f9592a7655809d88885aa111dfc105ab00825bb9
Rohit Tyagi <rohit.tyagi@thoughts2binary.com>
2024-03-10 13:20:46 +0530

    folfer read write

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import { readFileSync, writeFileSync } from "fs";
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: true, methods: "GET,HEAD,PUT,PATCH,POST,DELETE" }));
@@ -33,6 +33,26 @@ app.post("/api/v1/generate/text", async (req, res) => {
 
 app.get("/", async (req, res) => {
   res.send({ success: true });
+});
+
+app.get("/api/vi/get-bio", async (req, res) => {
+  try {
+    const file = readFileSync("./sample.txt", "utf8").toString();
+    res.send(file);
+  } catch (error) {}
+});
+
+app.put("/api/vi/put-bio", async (req, res) => {
+  try {
+    console.log("req.body", req.body.text);
+    const { text } = req.body;
+    if (!text || text === "") {
+      res.send({ success: false, msg: "text is empty" });
+      return;
+    }
+    writeFileSync("./sample.txt", text);
+    res.send({ success: true, text });
+  } catch (error) {}
 });
 
 app.listen(5000, () => {
