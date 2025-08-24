712771f203e435133bf1fc660fbc9b4348da2857
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-08-24 12:05:34 +0530

    path fix

--- constants/summery.js
+++ constants/summery.js
@@ -1,9 +1,17 @@
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function info() {
-  return fs.readFileSync("../sample.txt", "utf8");
+  const filePath = path.join(__dirname, "../sample.txt");
+  return fs.readFileSync(filePath, "utf8");
 }
 
 const localSummary = info();
+
+console.log(localSummary);
 
 export { localSummary };
