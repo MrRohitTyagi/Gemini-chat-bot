74896833d5189cd94faef390c02a9a3857c283f7
rohit-tyagi-brevo <rohit.tyagi@brevo.com>
2025-08-24 11:54:55 +0530

    post aded

--- api/index.js
+++ api/index.js
@@ -39,5 +39,7 @@ app.get("/", async (req, res) => {
   });
 });
 
-
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
 export default app;
